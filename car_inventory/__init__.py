3b98c4dab11241aec038287032cc6372cc390e2b
wattsjon2 <wattsjon2@gmail.com>
2021-11-09 20:03:07 -0500

    update 11/9/2021 - added sign up and db connection

--- car_inventory/__init__.py
+++ car_inventory/__init__.py
@@ -1,14 +1,22 @@
 from flask import Flask
 from config import Config
 from .site.routes import site
-from .authentication.routes import auth
 from .api.routes import api
+from .authentication.routes import auth
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from .models import db as root_db
 
 app = Flask(__name__)
 
-
 app.register_blueprint(site)
-app.register_blueprint(auth)
 app.register_blueprint(api)
+app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+
+migrate = Migrate(app, root_db)
--- config.py
+++ config.py
@@ -1,10 +1,13 @@
 import os
+from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # give access to the project in ANY OS we find ourselves in
 # Aloow outside files/folders to be added to  the project
 # base directory
+
+load_dotenv(os.path.join(basedir,'.env'))
 
 class Config():
     """
@@ -13,6 +16,8 @@ class Config():
         create config variables if not done already
     
     """
-    SECRECT_KEY = os.environ.get('SECRECT_KEY') or 'You will never guess'
+    SECRET_KEY = os.environ.get('SECRECT_KEY') or 'You will never guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False # turn off messages for updates in sqlalchemy
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # turn off messages for updates in sqlalchemy
